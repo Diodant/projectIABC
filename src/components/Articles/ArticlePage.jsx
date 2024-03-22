fd9bea6af452012e2accc358c524878eddc5ed97
Nick Tolok <97984634+Diodant@users.noreply.github.com>
2024-03-22 22:42:35 +0200

    add

--- src/components/Articles/ArticlePage.jsx
+++ src/components/Articles/ArticlePage.jsx
@@ -11,23 +11,42 @@ function ArticlePage() {
   }
 
   return (
-    <div className="xContent p-box py-6">
-      <h1 className="title is-font-title-regular has-text-centered has-text-var-blue">{article.title}</h1>
-      <div className="content wysiwyg">
-      <p className='has-text-centered p-style p-1'>{article.subtitle}</p>
-      <p>{article.content1}</p>
-      <p>{article.content2}</p>
-      <p>{article.content3}</p>
-      <p>{article.content4}</p>
-      <p>{article.content5}</p>
-      <p>{article.content6}</p>
-      <p>{article.content7}</p>
-      <p>{article.content8}</p>
-      <p>{article.content9}</p>
-      <p>{article.content10}</p>
-      <p>{article.content11}</p>
-      <p>{article.content12}</p>
-      <p>{article.content13}</p>
+    <div className="articlepage-wrapper">
+        <div className="text-center">
+        <div className="page-title">Статьи IABC</div>
+        </div>
+      <h1 className="articlepage-title">{article.title}</h1>
+      <div className="articlepage-content">
+      <div className='articlepage-summary'><b>{article.summary}</b></div>
+      <p className='articlepage-text fs-35 mb-30'>{article.content1}</p>
+      <p className='fs-25'>{article.content2}</p>
+      <p className='fs-25'>{article.content3}</p>
+      <p className='fs-25'>{article.content4}</p>
+      <p className='fs-25'>{article.content5}</p>
+      <p className='fs-25'>{article.content6}</p>
+      <p className='fs-25'>{article.content7}</p>
+      <p className='fs-25'>{article.content8}</p>
+      <p className='fs-25'>{article.content9}</p>
+      <p className='fs-25'>{article.content10}</p>
+      <p className='fs-25'>{article.content11}</p>
+      <p className='fs-25'>{article.content12}</p>
+      <p className='fs-25'>{article.content13}</p>
+      <p className='fs-25'>{article.content14}</p>
+      <p className='fs-25'>{article.content15}</p>
+      <p className='fs-25'>{article.content16}</p>
+      <p className='fs-25'>{article.content17}</p>
+      <p className='fs-25'>{article.content18}</p>
+      <p className='fs-25'>{article.content19}</p>
+      <p className='fs-25'>{article.content20}</p>
+      <p className='fs-25'>{article.content21}</p>
+      <p className='fs-25'>{article.content22}</p>
+      <p className='fs-25'>{article.content23}</p>
+      <p className='fs-25'>{article.content24}</p>
+      <p className='fs-25'>{article.content25}</p>
+      <p className='fs-25'>{article.content26}</p>
+      <p className='fs-25'>{article.content27}</p>
+      <p className='fs-25'>{article.content28}</p>
+
       </div>
     </div>
   );
