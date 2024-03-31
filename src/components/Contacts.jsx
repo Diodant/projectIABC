7da3c137f3ed69a8930d15197109b14de9c06563
Nick Tolok <97984634+Diodant@users.noreply.github.com>
2024-03-31 17:24:39 +0300

    add

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 function Contacts() {
   return (
     <div className="contact-wrapper">
+          <div className="text-center">
+        <div id="our-statute" className="contact-title" >Некоммерческая организация Международная Ассоциация Красоты и Ухода</div>
+
+        </div>
     <div className="contact-container">
-      <h1 className="contact-heading">Контакты</h1>
+      <h1 className="contact-heading">Юридический/Фактический адрес</h1>
       <div className="contact-details">
-        <p className="contact-item">Телефон: <span className="contact-info">+7 8 (800) 550-32-18</span></p>
-        <p className="contact-item">Email: <a href="mailto:info@mapik-ibi.ru" className="contact-info">info@mapik-ibi.ru</a></p>
-        <p className="contact-item">Адрес: <span className="contact-info">117405, Россия, Москва, Варшавское шоссе, 21 км, вл.4</span></p>
+        <p className="contact-item">Телефон: <span className="contact-info">8 (800) 550-48-76</span></p>
+        <p className="contact-item">Email: <a href="mailto:info@mapik-ibi.ru" className="contact-info">internationalasbeautyandcare@gmail.com </a></p>
+        <p className="contact-item">Адрес: <span className="contact-info">117405, Россия, Москва, Варшавское шоссе, 21 км</span></p>
       </div>
     </div>
     </div>
